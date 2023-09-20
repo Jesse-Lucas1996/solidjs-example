ea3133eecaed20ce2083cbe83859b24fd35fae2f
JesseLucas <jesse.lucas@indigosolutions.org.au>
2023-09-20 13:54:11 +0800

    optimised fetch call

--- src/routes/Cats.tsx
+++ src/routes/Cats.tsx
@@ -1,53 +1,38 @@
-import { createSignal, type Component, Switch, Match } from "solid-js";
+import { createSignal, type Component, createResource, For } from "solid-js";
 import { BiSolidCat } from "solid-icons/bi";
 import { Loading } from "../components/loader";
 
-const Cats: Component = () => {
-  interface State {
-    status: "pending" | "resolved" | "rejected" | "";
-    data?: any;
-    error?: any;
-  }
+type CatFact = {
+  fact: string;
+  error?: any;
+};
 
-  type CatFact = {
-    fact: string;
-    error?: any;
-  };
-  const [state, setState] = createSignal<State>({
-    status: "",
-    data: null,
-    error: null,
-  });
+const Cats: Component = () => {
   const fetchCatFact = async () => {
-    try {
-      setState({ status: "pending" });
-      const res: CatFact = await (
-        await fetch("https://catfact.ninja/fact")
-      ).json();
-      setState({ status: "resolved", data: res.fact });
-    } catch (error: any) {
-      setState({ status: "rejected", error: error });
-    }
+    const resp = await fetch("https://catfact.ninja/fact");
+    const catFact = (await resp.json()) as CatFact;
+    return catFact.fact;
   };
+
+  const [fact, { refetch }] = createResource(fetchCatFact);
 
   return (
     <div class="bg-slate-500 min-h-screen flex flex-col items-center justify-center text-white text-3xl">
       <button
         class="h-24 w-24 p-2 m-2 text-lg text-indigo-100 transition-transform duration-150 rounded-full focus:shadow-outline hover:scale-110 transform hover:rotate-12"
-        onclick={fetchCatFact}
+        onClick={refetch}
       >
         <BiSolidCat fill="#FFFFFF" class="h-full w-full" />
       </button>
       <br />
-      <Switch fallback={<div>Click the kitty</div>}>
-        <Match when={state().status === "pending"}>
-          <Loading />
-        </Match>
-        <Match when={state().status === "rejected"}>{state().error}</Match>
-        <Match when={state().status === "resolved"}>{state().data}</Match>
-      </Switch>
+      <div>
+        <div>
+          {fact.loading && <Loading />}
+          {fact.error && <p>Ruh Roh Raggy Something went wrong</p>}
+          {!fact.loading && fact() && <p>{fact() ?? ""}</p>}
+        </div>
+      </div>
     </div>
   );
 };
-
 export default Cats;
